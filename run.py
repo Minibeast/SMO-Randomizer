da2085f7336110d18df05801b523bd4b41bd0dec
Minibeast <minibossemail@gmail.com>
2020-08-01 23:37:07 -0400

    html file + hints added

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ import random
 import configparser
 import urllib.request
 
-version = "1.1.3"
+version = "1.1.4"
 
 try:
     moons = open('moons.json')
@@ -26,12 +26,12 @@ except FileNotFoundError:
     moons = moons['results']
 
 randomizer = open("randomizer.txt", "w+")
+htmlrandomizer = open("randomizer.html", "w+")
 collectedMoons = []
 overrideArray = []
 
-requiredCoins = 0
-deepWoods = 0
-outfits = 0
+coin_moons = [217, 283, 329, 412, 453, 528, 598, 658, 740]
+purple_moons = [228, 285, 342, 465, 466, 536, 537, 604, 660, 661, 742, 743]
 
 settings = configparser.ConfigParser()
 settings.read('settings.ini')
@@ -72,6 +72,14 @@ else:
     seed = seed_option
     random.seed(seed)
 
+increment = 0
+
+
+def checkbox_generate(text):
+    global increment
+    increment += 1
+    return '<div class="checkbox"><input type="checkbox" id="' + str(increment) + '"/>\n<label class="strikethrough" for="' + str(increment) + '">' + str(text) + '</label>\n</div>\n'
+
 
 def randomize(min, max):
     value = rand(min, max)
@@ -94,7 +102,15 @@ def generate(min, max, prerequisite, amount):
             if settings.getboolean('Overrides', str(x['id'])):
                 if (x["moonPrerequisites"] is None or x["moonPrerequisites"][0]["id"] <= prerequisite) and (
                         x["id"] not in collectedMoons):
-                    randomizer.write(x["name"] + "\n")
+                    trait = ""
+                    if x["id"] in coin_moons:
+                        trait = " [100 Coins]"
+                    elif x["id"] in purple_moons:
+                        trait = " [Outfit Moon]"
+                    elif x["id"] == 339:
+                        trait = " [500 Coins]"
+                    randomizer.write(x["name"] + trait + "\n")
+                    htmlrandomizer.write(checkbox_generate(x["name"] + "<b>" + trait + "</b>"))
                     collectedMoons.append(x["id"])
                     i += 1
             else:
@@ -105,7 +121,15 @@ def generate(min, max, prerequisite, amount):
                 x["isStoryMoon"] is not True) and (x["moonTypes"] is None or (
                 x["moonTypes"][0]["name"] != "Hint Art" and x["moonTypes"][0]["name"] != "Tourist")) and (
                 x["id"] not in collectedMoons):
-            randomizer.write(x["name"] + "\n")
+            trait = ""
+            if x["id"] in coin_moons:
+                trait = " [100 Coins]"
+            elif x["id"] in purple_moons:
+                trait = " [Outfit Moon]"
+            elif x["id"] == 339:
+                trait = " [500 Coins]"
+            randomizer.write(x["name"] + trait + "\n")
+            htmlrandomizer.write(checkbox_generate(x["name"] + "<b>" + trait + "</b>"))
             collectedMoons.append(x["id"])
             i += 1
 
@@ -114,18 +138,29 @@ date = datetime.datetime.now()
 
 randomizer.write("SMO Randomizer generated on " + date.strftime("%b") + " " + date.strftime("%d") + " " + date.strftime("%Y") + " at " + date.strftime("%I") + ":" + date.strftime("%M") + " " + date.strftime("%p") + " (Version: " + version + ")")
 randomizer.write("\nGenerated Seed: " + str(seed) + "\n")
+
+htmlrandomizer.write("<!DOCTYPE html>\n<head>\n<title>Seed: " + str(seed) + "</title>\n<style>\nlabel.strikethrough{\n\tline-height: 1.8;\n}\ninput[type=checkbox]:checked + label.strikethrough{\n\ttext-decoration: line-through;\n}\n</style>\n</head><body>\n")
+htmlrandomizer.write("<h3>Generated on: " + date.strftime("%b") + " " + date.strftime("%d") + " " + date.strftime("%Y") + " at " + date.strftime("%I") + ":" + date.strftime("%M") + " " + date.strftime("%p") + " (Version: " + version + ")")
+
 if len(overrideArray) > 0:
     randomizer.write("\nOVERRIDES:\n")
+    htmlrandomizer.write("\n<h2>Overrides</h2>\n<ul>\n")
 
 for x in overrideArray:
     randomizer.write(moons[int(x) - 1]["name"] + " = " + settings["Overrides"][x] + "\n")
+    htmlrandomizer.write("<li>" + moons[int(x) - 1]["name"] + " = " + settings["Overrides"][x] + "</li>\n")
+
+if len(overrideArray) > 0:
+    htmlrandomizer.write("</ul>")
 
 moonCount = 0
 
 # Cascade
 randomizer.write("\nCASCADE:\n")
+htmlrandomizer.write("\n<h2>Cascade</h2>\n")
 
 randomizer.write(moons[135]["name"] + "\n")
+htmlrandomizer.write(checkbox_generate(moons[135]["name"]))
 
 local = rand(moonCount, 1)
 moonCount += local
@@ -133,6 +168,7 @@ moonCount += local
 generate(135, 174, 136, local)
 
 randomizer.write(moons[136]["name"] + " [3]\n")
+htmlrandomizer.write(checkbox_generate(moons[136]["name"] + " [3]"))
 
 generate(135, 174, 137, 1 - moonCount)
 #
@@ -141,6 +177,7 @@ generate(135, 174, 137, 1 - moonCount)
 moonCount = 0
 
 randomizer.write("\nSAND: \n")
+htmlrandomizer.write("\n<h2>Sand</h2>\n")
 
 local = rand(0, 16)
 moonCount += local
@@ -149,6 +186,7 @@ generate(175, 263, 0, local)
 
 if moonCount < 16:
     randomizer.write(moons[175]["name"] + "\n")
+    htmlrandomizer.write(checkbox_generate(moons[175]["name"]))
     moonCount += 1
 
     local = rand(0, 16 - moonCount)
@@ -159,6 +197,7 @@ if moonCount < 16:
 if not peaceSkips:
     if moonCount < 16:
         randomizer.write(moons[176]["name"] + "\n")
+        htmlrandomizer.write(checkbox_generate(moons[176]["name"]))
         moonCount += 1
 
         local = rand(0, 16 - moonCount)
@@ -168,6 +207,7 @@ if not peaceSkips:
 
         if moonCount < 14:
             randomizer.write(moons[177]["name"] + " [3]\n")
+            htmlrandomizer.write(checkbox_generate(moons[177]["name"] + " [3]"))
             moonCount += 3
 
             local = rand(0, 16 - moonCount)
@@ -177,6 +217,7 @@ if not peaceSkips:
 
             if moonCount < 14:
                 randomizer.write(moons[178]["name"] + " [3]\n")
+                htmlrandomizer.write(checkbox_generate(moons[178]["name"] + " [3]"))
                 moonCount += 3
 
                 generate(175, 263, 179, 16 - moonCount)
@@ -188,6 +229,7 @@ if not peaceSkips:
 else:
     if moonCount < 14:
         randomizer.write(moons[177]["name"] + " [3]\n")
+        htmlrandomizer.write(checkbox_generate(moons[177]["name"] + " [3]"))
         moonCount += 3
 
         local = rand(0, 16 - moonCount)
@@ -197,6 +239,7 @@ else:
 
         if moonCount < 14:
             randomizer.write(moons[178]["name"] + " [3]\n")
+            htmlrandomizer.write(checkbox_generate(moons[178]["name"] + " [3]"))
             moonCount += 3
 
             generate(175, 263, 179, 16 - moonCount)
@@ -212,6 +255,7 @@ else:
 moonCount = 0
 
 randomizer.write("\nLAKE: \n")
+htmlrandomizer.write("\n<h2>Lake</h2>\n")
 
 local = rand(0, 8)
 moonCount += local
@@ -220,6 +264,7 @@ generate(264, 305, 0, local)
 
 if moonCount < 6:
     randomizer.write(moons[264]["name"] + " [3]\n")
+    htmlrandomizer.write(checkbox_generate(moons[264]["name"] + " [3]"))
     moonCount += 3
 
     generate(264, 305, 265, 8 - moonCount)
@@ -231,6 +276,7 @@ elif 8 > moonCount >= 6:
 moonCount = 0
 
 randomizer.write("\nWOODED: \n")
+htmlrandomizer.write("\n<h2>Wooded</h2>\n")
 
 local = rand(0, 16)
 moonCount += local
@@ -239,6 +285,7 @@ generate(306, 381, 0, local)
 
 if moonCount < 16:
     randomizer.write(moons[306]["name"] + "\n")
+    htmlrandomizer.write(checkbox_generate(moons[306]["name"]))
     moonCount += 1
 
     local = rand(0, 16 - moonCount)
@@ -248,6 +295,7 @@ if moonCount < 16:
 
     if moonCount < 14:
         randomizer.write(moons[307]["name"] + " [3]\n")
+        htmlrandomizer.write(checkbox_generate(moons[307]["name"] + " [3]"))
         moonCount += 3
 
         local = rand(0, 16 - moonCount)
@@ -257,6 +305,7 @@ if moonCount < 16:
 
         if moonCount < 16:
             randomizer.write(moons[308]["name"] + "\n")
+            htmlrandomizer.write(checkbox_generate(moons[308]["name"]))
             moonCount += 1
 
             local = rand(0, 16 - moonCount)
@@ -266,6 +315,7 @@ if moonCount < 16:
 
             if moonCount < 14:
                 randomizer.write(moons[309]["name"] + " [3]\n")
+                htmlrandomizer.write(checkbox_generate(moons[309]["name"] + " [3]"))
                 moonCount += 3
 
                 generate(306, 381, 310, 16 - moonCount)
@@ -277,6 +327,7 @@ if moonCount < 16:
 
 # Lost
 randomizer.write("\nLOST: \n")
+htmlrandomizer.write("\n<h2>Lost</h2>\n")
 
 generate(391, 425, 0, 10)
 #
@@ -285,6 +336,7 @@ generate(391, 425, 0, 10)
 moonCount = 0
 
 randomizer.write("\nMETRO: \n")
+htmlrandomizer.write("\n<h2>Metro</h2>\n")
 
 local = rand(0, 5)
 moonCount += local
@@ -292,6 +344,7 @@ moonCount += local
 generate(426, 506, 0, local)
 
 randomizer.write(moons[426]["name"] + " [3]\n")
+htmlrandomizer.write(checkbox_generate(moons[426]["name"] + " [3]"))
 moonCount += 3
 
 local = rand(0, 20 - moonCount)
@@ -300,7 +353,9 @@ moonCount += local
 generate(426, 506, 427, local)
 
 if moonCount < 20:
-    randomizer.write(generatestory(427, 430) + "\n")
+    story = generatestory(427, 430)
+    randomizer.write(story + "\n")
+    htmlrandomizer.write(checkbox_generate(story))
     moonCount += 1
 
     local = rand(0, 20 - moonCount)
@@ -309,7 +364,9 @@ if moonCount < 20:
     generate(426, 506, 427, local)
 
     if moonCount < 20:
-        randomizer.write(generatestory(427, 430) + "\n")
+        story = generatestory(427, 430)
+        randomizer.write(story + "\n")
+        htmlrandomizer.write(checkbox_generate(story))
         moonCount += 1
 
         local = rand(0, 20 - moonCount)
@@ -318,7 +375,9 @@ if moonCount < 20:
         generate(426, 506, 427, local)
 
         if moonCount < 20:
-            randomizer.write(generatestory(427, 430) + "\n")
+            story = generatestory(427, 430)
+            randomizer.write(story + "\n")
+            htmlrandomizer.write(checkbox_generate(story))
             moonCount += 1
 
             local = rand(0, 20 - moonCount)
@@ -327,7 +386,9 @@ if moonCount < 20:
             generate(426, 506, 427, local)
 
             if moonCount < 20:
-                randomizer.write(generatestory(427, 430) + "\n")
+                story = generatestory(427, 430)
+                randomizer.write(story + "\n")
+                htmlrandomizer.write(checkbox_generate(story))
                 moonCount += 1
 
                 local = rand(0, 20 - moonCount)
@@ -337,6 +398,7 @@ if moonCount < 20:
 
                 if moonCount < 20:
                     randomizer.write(moons[431]["name"] + "\n")
+                    htmlrandomizer.write(checkbox_generate(moons[431]["name"]))
                     moonCount += 1
 
                     local = rand(0, 20 - moonCount)
@@ -346,6 +408,7 @@ if moonCount < 20:
 
                     if moonCount < 18:
                         randomizer.write(moons[432]["name"] + " [3]\n")
+                        htmlrandomizer.write(checkbox_generate(moons[432]["name"] + " [3]"))
                         moonCount += 3
 
                         generate(426, 506, 433, 20 - moonCount)
@@ -358,6 +421,7 @@ if moonCount < 20:
 moonCount = 0
 
 randomizer.write("\nSNOW: \n")
+htmlrandomizer.write("\n<h2>Snow</h2>\n")
 
 local = rand(0, 10)
 moonCount += local
@@ -366,7 +430,9 @@ generate(507, 561, 0, local)
 
 if not peaceSkips:
     if moonCount < 10:
-        randomizer.write(generatestory(507, 510) + "\n")
+        story = generatestory(507, 510)
+        randomizer.write(story + "\n")
+        htmlrandomizer.write(checkbox_generate(story))
         moonCount += 1
 
         local = rand(0, 10 - moonCount)
@@ -375,7 +441,9 @@ if not peaceSkips:
         generate(507, 561, 0, local)
 
         if moonCount < 10:
-            randomizer.write(generatestory(507, 510) + "\n")
+            story = generatestory(507, 510)
+            randomizer.write(story + "\n")
+            htmlrandomizer.write(checkbox_generate(story))
             moonCount += 1
 
             local = rand(0, 10 - moonCount)
@@ -384,7 +452,9 @@ if not peaceSkips:
             generate(507, 561, 0, local)
 
             if moonCount < 10:
-                randomizer.write(generatestory(507, 510) + "\n")
+                story = generatestory(507, 510)
+                randomizer.write(story + "\n")
+                htmlrandomizer.write(checkbox_generate(story))
                 moonCount += 1
 
                 local = rand(0, 10 - moonCount)
@@ -393,7 +463,9 @@ if not peaceSkips:
                 generate(507, 561, 0, local)
 
                 if moonCount < 10:
-                    randomizer.write(generatestory(507, 510) + "\n")
+                    story = generatestory(507, 510)
+                    randomizer.write(story + "\n")
+                    htmlrandomizer.write(checkbox_generate(story))
                     moonCount += 1
 
                     local = rand(0, 10 - moonCount)
@@ -403,6 +475,7 @@ if not peaceSkips:
 
                     if moonCount < 8:
                         randomizer.write(moons[511]["name"] + " [3]\n")
+                        htmlrandomizer.write(checkbox_generate(moons[511]["name"] + " [3]"))
                         moonCount += 3
 
                         generate(507, 561, 512, 10 - moonCount)
@@ -411,6 +484,7 @@ if not peaceSkips:
 else:
     if moonCount < 8:
         randomizer.write(moons[511]["name"] + " [3]\n")
+        htmlrandomizer.write(checkbox_generate(moons[511]["name"] + " [3]"))
         moonCount += 3
 
         generate(507, 561, 512, 10 - moonCount)
@@ -422,6 +496,7 @@ else:
 moonCount = 0
 
 randomizer.write("\nSEASIDE: \n")
+htmlrandomizer.write("\n<h2>Seaside</h2>\n")
 
 local = rand(0, 10)
 moonCount += local
@@ -429,7 +504,9 @@ moonCount += local
 generate(562, 632, 0, local)
 
 if moonCount < 10:
-    randomizer.write(generatestory(562, 565) + "\n")
+    story = generatestory(562, 565)
+    randomizer.write(story + "\n")
+    htmlrandomizer.write(checkbox_generate(story))
     moonCount += 1
 
     local = rand(0, 10 - moonCount)
@@ -438,7 +515,9 @@ if moonCount < 10:
     generate(562, 632, 0, local)
 
     if moonCount < 10:
-        randomizer.write(generatestory(562, 565) + "\n")
+        story = generatestory(562, 565)
+        randomizer.write(story + "\n")
+        htmlrandomizer.write(checkbox_generate(story))
         moonCount += 1
 
         local = rand(0, 10 - moonCount)
@@ -447,7 +526,9 @@ if moonCount < 10:
         generate(562, 632, 0, local)
 
         if moonCount < 10:
-            randomizer.write(generatestory(562, 565) + "\n")
+            story = generatestory(562, 565)
+            randomizer.write(story + "\n")
+            htmlrandomizer.write(checkbox_generate(story))
             moonCount += 1
 
             local = rand(0, 10 - moonCount)
@@ -456,7 +537,9 @@ if moonCount < 10:
             generate(562, 632, 0, local)
 
             if moonCount < 10:
-                randomizer.write(generatestory(562, 565) + "\n")
+                story = generatestory(562, 565)
+                randomizer.write(story + "\n")
+                htmlrandomizer.write(checkbox_generate(story))
                 moonCount += 1
 
                 local = rand(0, 10 - moonCount)
@@ -466,6 +549,7 @@ if moonCount < 10:
 
                 if moonCount < 8:
                     randomizer.write(moons[566]["name"] + " [3]\n")
+                    htmlrandomizer.write(checkbox_generate(moons[566]["name"] + " [3]"))
                     moonCount += 3
 
                     generate(562, 632, 567, local)
@@ -478,6 +562,7 @@ if moonCount < 10:
 moonCount = 0
 
 randomizer.write("\nLUNCHEON: \n")
+htmlrandomizer.write("\n<h2>Luncheon</h2>\n")
 
 local = rand(0, 4)
 moonCount += local
@@ -485,6 +570,7 @@ moonCount += local
 generate(633, 700, 0, local)
 
 randomizer.write(moons[633]["name"] + "\n")
+htmlrandomizer.write(checkbox_generate(moons[633]["name"]))
 moonCount += 1
 
 local = rand(0, 14)
@@ -494,6 +580,7 @@ generate(633, 700, 634, local)
 
 if moonCount < 18:
     randomizer.write(moons[634]["name"] + "\n")
+    htmlrandomizer.write(checkbox_generate(moons[634]["name"]))
     moonCount += 1
 
     local = rand(0, 18 - moonCount)
@@ -503,6 +590,7 @@ if moonCount < 18:
 
     if moonCount < 16:
         randomizer.write(moons[635]["name"] + " [3]\n")
+        htmlrandomizer.write(checkbox_generate(moons[635]["name"] + " [3]"))
         moonCount += 3
 
         local = rand(0, 18 - moonCount)
@@ -512,6 +600,7 @@ if moonCount < 18:
 
         if moonCount < 18:
             randomizer.write(moons[636]["name"] + "\n")
+            htmlrandomizer.write(checkbox_generate(moons[636]["name"]))
             moonCount += 1
 
             local = rand(0, 18 - moonCount)
@@ -521,6 +610,7 @@ if moonCount < 18:
 
             if moonCount < 16:
                 randomizer.write(moons[637]["name"] + " [3]\n")
+                htmlrandomizer.write(checkbox_generate(moons[637]["name"] + " [3]"))
                 moonCount += 3
 
                 generate(633, 700, 638, 18 - moonCount)
@@ -535,11 +625,16 @@ if moonCount < 18:
 moonCount = 6
 
 randomizer.write("\nBOWSERS: \n")
+htmlrandomizer.write("\n<h2>Bowsers</h2>\n")
 
 randomizer.write(moons[711]["name"] + "\n")
+htmlrandomizer.write(checkbox_generate(moons[711]["name"]))
 randomizer.write(moons[712]["name"] + "\n")
+htmlrandomizer.write(checkbox_generate(moons[712]["name"]))
 randomizer.write(moons[713]["name"] + "\n")
+htmlrandomizer.write(checkbox_generate(moons[713]["name"]))
 randomizer.write(moons[714]["name"] + " [3]\n")
+htmlrandomizer.write(checkbox_generate(moons[714]["name"] + " [3]"))
 
 sprinkleID = 715
 
@@ -549,3 +644,5 @@ if bowserSprinkle:
 generate(711, 772, sprinkleID, 8 - moonCount)
 
 randomizer.close()
+htmlrandomizer.write("</body>")
+htmlrandomizer.close()
